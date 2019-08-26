8b40ee7eb34bc1260335bf960d7e25054422c25d
Arnold Restrepo <arnold.restrepo@quick.com.co>
2019-08-25 21:27:04 -0500

    Webpack Development Mode Config

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require("path");
+
+const babel = {
+  test: /\.(js|jsx)$/,
+  exclude: /node_modules/,
+  use: {
+    loader: "babel-loader"
+  }
+};
+
+const config = {
+  mode: "development",
+  entry: {
+    index: "./src/index.js"
+  },
+  output: {
+    filename: "[name].js",
+    path: path.join(__dirname, "dist/js")
+  },
+  devServer: {
+    contentBase: "./dist",
+    historyApiFallback: {
+      index: "index.html"
+    },
+    port: 9000
+  },
+  module: {
+    rules: [babel]
+  }
+};
+
+module.exports = config;
