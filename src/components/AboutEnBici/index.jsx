43feb27e7df3d438ffc5c6fc5230e817c5bdc9e1
Arnold Restrepo <restrepo_arnold@outlook.com>
2020-01-01 21:04:12 -0500

    About Description Change Design

--- src/components/AboutEnBici/index.jsx
+++ src/components/AboutEnBici/index.jsx
@@ -1,24 +1,37 @@
-import React from "react";
-import { AboutWrapper, AboutTitle, AboutDescription } from "./style";
+import React from 'react';
+import {
+  AboutWrapper,
+  AboutTitle,
+  AboutDescription,
+  AboutDescriptionWrapper,
+  AboutImageWrapper,
+  AboutImage
+} from './style';
+import { IMG_PATH } from '../../helpers';
 
 const AboutEnBici = () => {
   return (
     <AboutWrapper>
-      <AboutTitle>¿Por qué En Bici?</AboutTitle>
-      <AboutDescription>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium
-        minima consectetur delectus dolor repellat optio inventore aut ut!
-        Dolorum et perspiciatis reprehenderit amet odio. Laudantium mollitia,
-        repellat quisquam amet ex quam, quasi doloribus a sint provident
-        molestiae ea ratione suscipit.
-      </AboutDescription>
-      <AboutDescription>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium
-        minima consectetur delectus dolor repellat optio inventore aut ut!
-        Dolorum et perspiciatis reprehenderit amet odio. Laudantium mollitia,
-        repellat quisquam amet ex quam, quasi doloribus a sint provident
-        molestiae ea ratione suscipit.
-      </AboutDescription>
+      <AboutDescriptionWrapper>
+        <AboutTitle>¿Por qué En Bici?</AboutTitle>
+        <AboutDescription>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium
+          minima consectetur delectus dolor repellat optio inventore aut ut!
+          Dolorum et perspiciatis reprehenderit amet odio. Laudantium mollitia,
+          repellat quisquam amet ex quam, quasi doloribus a sint provident
+          molestiae ea ratione suscipit.
+        </AboutDescription>
+        <AboutDescription>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium
+          minima consectetur delectus dolor repellat optio inventore aut ut!
+          Dolorum et perspiciatis reprehenderit amet odio. Laudantium mollitia,
+          repellat quisquam amet ex quam, quasi doloribus a sint provident
+          molestiae ea ratione suscipit.
+        </AboutDescription>
+      </AboutDescriptionWrapper>
+      <AboutImageWrapper>
+        <AboutImage src={`${IMG_PATH}plans-background.jpg`} alt='' />
+      </AboutImageWrapper>
     </AboutWrapper>
   );
 };
--- src/components/AboutEnBici/style.js
+++ src/components/AboutEnBici/style.js
@@ -1,12 +1,19 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 export const AboutWrapper = styled.section`
   width: 100%;
-  height: 500px;
+  height: auto;
+  display: grid;
+  grid-template-columns: 1fr 300px;
+  grid-gap: 0 1rem;
   overflow: hidden;
-  padding: 3rem 1rem;
-  background: var(--color-white) url("./img/background-about-en-bici.png") top
-    center/cover no-repeat fixed;
+  padding: 3rem 2rem;
+  background: var(--color-white);
+  @media screen and (max-width: 768px) {
+    grid-template-columns: 1fr;
+    grid-gap: 2rem 0;
+    grid-template-rows: auto;
+  }
 `;
 
 export const AboutTitle = styled.h2`
@@ -14,12 +21,26 @@ export const AboutTitle = styled.h2`
   margin: 0 0 2rem 0;
 `;
 
+export const AboutDescriptionWrapper = styled.div``;
+
 export const AboutDescription = styled.p`
   font-size: 1rem;
-  color: var(--color-white);
-  width: 80%;
+  color: var(--color-black);
   line-height: 1.3;
   margin-bottom: 1rem;
-  padding: 1rem;
-  background: var(--color-black);
+  /* background: var(--color-black); */
+`;
+
+export const AboutImageWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const AboutImage = styled.img`
+  width: 250px;
+  height: 250px;
+  background-clip: content-box;
+  border-radius: 50%;
+  box-shadow: inset 0px 0px 5px 3px rgba(0, 0, 0, 0.25);
 `;
