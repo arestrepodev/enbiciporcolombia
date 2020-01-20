6627eb7cb0cf3625863ca97ff9229f01a698be8b
root <root@LAPTOP-K6OELS4R.localdomain>
2020-01-19 19:47:14 -0500

    Destinations

---
+++ src/components/Destinations/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import StyledTitle from '../StyledTitle';
+
+const Destinations = () => (
+  <section>
+    <StyledTitle subtitle='Tenemos Muchos' title='Destinos' />
+  </section>
+);
+
+export default Destinations;
