92c31e3b8d8bae83de339184f0cf86171e9132bf
Arnold Restrepo <restrepo_arnold@outlook.com>
2020-04-15 10:16:06 -0500

    Top Bar - Create

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,19 +1,48 @@
 import React from 'react';
 import { Link } from '@reach/router';
-import { WrapperHeader, HeaderComponent } from './style';
+import { MdPermPhoneMsg } from 'react-icons/md';
+import { FaWhatsapp } from 'react-icons/fa';
+import {
+  WrapperHeader,
+  HeaderComponent,
+  TopBar,
+  Contact,
+  Language,
+} from './style';
 import Logo from '../Logo';
 import Menu from '../Menu';
 
 const Header = () => {
   return (
-    <WrapperHeader>
-      <HeaderComponent>
-        <Link to='/'>
-          <Logo width='80' />
-        </Link>
-        <Menu />
-      </HeaderComponent>
-    </WrapperHeader>
+    <>
+      <TopBar>
+        <Contact>
+          <a href='https://wa.link/yvwpyc'>
+            <FaWhatsapp />
+            Quiero Cotizar
+          </a>
+          <a href='https://wa.link/yvwpyc'>
+            <MdPermPhoneMsg />
+            312 4679089 - CO
+          </a>
+          <a href='https://wa.link/80vyix'>
+            <MdPermPhoneMsg />
+            416 7748900 - CA
+          </a>
+        </Contact>
+        <Language>
+          <a href='#'>English</a>
+        </Language>
+      </TopBar>
+      <WrapperHeader>
+        <HeaderComponent>
+          <Link to='/'>
+            <Logo width='80' />
+          </Link>
+          <Menu />
+        </HeaderComponent>
+      </WrapperHeader>
+    </>
   );
 };
 
--- src/components/Header/style.js
+++ src/components/Header/style.js
@@ -11,3 +11,38 @@ export const HeaderComponent = styled.header`
   grid-template-columns: 150px 2fr;
   align-items: center;
 `;
+
+export const TopBar = styled.div`
+  background: var(--color-black);
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  a {
+    color: white;
+  }
+`;
+
+export const Contact = styled.div`
+  padding-left: 1rem;
+  svg {
+    color: var(--color-blue);
+    margin-right: 5px;
+  }
+  a {
+    margin: 0 0.8rem;
+    text-decoration: none;
+  }
+
+  @media screen and(max-width: 596px) {
+    a:nth-last-child(-n + 2) {
+      display: none;
+    }
+  }
+`;
+export const Language = styled.div`
+  padding: 1rem;
+  background: var(--color-blue);
+  a {
+    text-decoration: none;
+  }
+`;
