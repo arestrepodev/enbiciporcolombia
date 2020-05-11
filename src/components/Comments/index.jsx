7d5933f86feecdff38dcb39e2ef3ed37dcd29607
Arnold Restrepo <restrepo_arnold@outlook.com>
2020-05-10 21:17:03 -0500

    Translate Comments into English

--- src/components/Comments/index.jsx
+++ src/components/Comments/index.jsx
@@ -1,26 +1,31 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { MdStarHalf } from 'react-icons/md';
+import { Context } from '../../Context';
 import {
   CommentsWrapper,
   CommentsTitle,
   CommentDescription,
   CommentsList,
   Comment,
-  CommentsDescription
+  CommentsDescription,
 } from './style';
 // eslint-disable-next-line import/no-unresolved
 import comments from '../../api/comments.json';
 
 const Comments = () => {
+  const { isEnglish } = useContext(Context);
   return (
     <CommentsWrapper>
-      <CommentsTitle>Mejores Comentarios</CommentsTitle>
+      <CommentsTitle>
+        {isEnglish ? 'Best Comments' : 'Mejores Comentarios'}
+      </CommentsTitle>
       <CommentsDescription>
-        Para nosotros es importante el impacto que tenemos en las personas, por
-        eso ellos nos recomiendan
+        {isEnglish
+          ? 'The impact we have on people is important to us, for that they recommend us.'
+          : 'Para nosotros es importante el impacto que tenemos en las personas, por eso ellos nos recomiendan.'}
       </CommentsDescription>
       <CommentsList>
-        {comments.map(comment => (
+        {comments.map((comment) => (
           <Comment key={comment.id}>
             <img src={comment.img} alt={`Comentario de ${comment.author}`} />
             <CommentDescription>
