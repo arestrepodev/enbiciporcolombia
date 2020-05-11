cf82fcf75a7f32300d7f330ea5f9bf3673200553
Arnold Restrepo <restrepo_arnold@outlook.com>
2020-05-11 17:03:50 -0500

    Translate Choose Component into English

--- src/components/Choose/index.jsx
+++ src/components/Choose/index.jsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { IMG_PATH } from '../../helpers';
+import { Context } from '../../Context';
 import {
   ChooseWrapper,
   ChooseHeader,
@@ -10,50 +11,56 @@ import {
   LinkOverlay,
 } from './style';
 
-const Choose = () => (
-  <ChooseWrapper>
-    <ChooseHeader>
-      <small>Escoge tu</small>
-      <h3>Aventura en Bici</h3>
-      <p>
-        Escoge tus rutas y disfruta del paisaje, nsotros nos encargaremos para
-        que pases las mejores vacaciones en compañia de{' '}
-        <strong>tú bicicleta.</strong>
-      </p>
-    </ChooseHeader>
+const Choose = () => {
+  const { isEnglish } = useContext(Context);
+  return (
+    <ChooseWrapper>
+      <ChooseHeader>
+        <small>{isEnglish ? 'Choose your' : 'Escoge tu'}</small>
+        <h3>{isEnglish ? 'Bike Adventure' : 'Aventura en Bici'}</h3>
+        <p>
+          {isEnglish
+            ? 'Choose your routes and enjoy the scenery, we take care of you to spend the best holidays with your bike.'
+            : 'Escoge tus rutas y disfruta del paisaje, nsotros nos encargaremos para que pases las mejores vacaciones en compañia de tú bicicleta.'}
+        </p>
+      </ChooseHeader>
 
-    <ChooseContent>
-      <ChooseList>
-        <li>
-          <Link to='/basic'>
-            <LinkImage src={`${IMG_PATH}plan-basic.jpg`} alt='Plan Basic' />
-            <LinkOverlay>
-              <h3>Plan Basic</h3>
-            </LinkOverlay>
-          </Link>
-        </li>
-        <li>
-          <Link to='/adventure'>
-            <LinkImage
-              src={`${IMG_PATH}plan-adventure.jpg`}
-              alt='Plan Adventure'
-            />
-            <LinkOverlay>
-              <h3>Plan Adventure</h3>
-            </LinkOverlay>
-          </Link>
-        </li>
-        <li>
-          <Link to='/extreme'>
-            <LinkImage src={`${IMG_PATH}plan-extreme.jpg`} alt='Plan Extreme' />
-            <LinkOverlay>
-              <h3>Plan Extreme</h3>
-            </LinkOverlay>
-          </Link>
-        </li>
-      </ChooseList>
-    </ChooseContent>
-  </ChooseWrapper>
-);
+      <ChooseContent>
+        <ChooseList>
+          <li>
+            <Link to='/basic'>
+              <LinkImage src={`${IMG_PATH}plan-basic.jpg`} alt='Plan Basic' />
+              <LinkOverlay>
+                <h3>{isEnglish ? 'Basic Plan' : 'Plan Basic'}</h3>
+              </LinkOverlay>
+            </Link>
+          </li>
+          <li>
+            <Link to='/adventure'>
+              <LinkImage
+                src={`${IMG_PATH}plan-adventure.jpg`}
+                alt='Plan Adventure'
+              />
+              <LinkOverlay>
+                <h3>{isEnglish ? 'Adventure Plan' : 'Plan Adventure'}</h3>
+              </LinkOverlay>
+            </Link>
+          </li>
+          <li>
+            <Link to='/extreme'>
+              <LinkImage
+                src={`${IMG_PATH}plan-extreme.jpg`}
+                alt='Plan Extreme'
+              />
+              <LinkOverlay>
+                <h3>{isEnglish ? 'Extreme Plan' : 'Plan Extreme'}</h3>
+              </LinkOverlay>
+            </Link>
+          </li>
+        </ChooseList>
+      </ChooseContent>
+    </ChooseWrapper>
+  );
+};
 
 export default Choose;
