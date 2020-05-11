d06ab60164ba2dbf1d8ec509f302c70e0f929712
Arnold Restrepo <restrepo_arnold@outlook.com>
2020-05-10 21:12:21 -0500

    Translate Exeriences into English

--- src/components/Experiences/index.jsx
+++ src/components/Experiences/index.jsx
@@ -1,23 +1,30 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { Context } from '../../Context';
 import { ExperiencesWrapper, ExperienceDescription, Video } from './style';
 import StyledTitle from '../StyledTitle';
 import { LinkButtonBlue } from '../../styles/Buttons';
 import { IMG_PATH } from '../../helpers';
 
 const Experiences = () => {
+  const { isEnglish } = useContext(Context);
   return (
     <ExperiencesWrapper>
-      <StyledTitle title='Experiencias' subtitle='Nuevas' />
+      <StyledTitle
+        title={isEnglish ? 'Experiences' : 'Experiencias'}
+        subtitle={isEnglish ? 'New' : 'Nuevas'}
+      />
       <Video controls muted>
         <source src={`${IMG_PATH}video-cover-en-bici.webm`} />
       </Video>
       <ExperienceDescription>
         <p>
-          Podrás descubrir Colombia montado en tu Bici, paisajes únicos,
-          aventuras nuevas y mucho más. Vive la experiencia más grande que
-          puedas vivir, que esperas...
+          {isEnglish
+            ? 'You can discover Colombia in your bike, at unique landscapes, adventures and more. Live the new experience you can live...'
+            : 'Podrás descubrir Colombia montado en tu Bici, paisajes únicos,aventuras nuevas y mucho más. Vive la experiencia más grande que puedas vivir, que esperas...'}
         </p>
-        <LinkButtonBlue to='/enbici'>Ver Más</LinkButtonBlue>
+        <LinkButtonBlue to='/enbici'>
+          {isEnglish ? 'Read More' : 'Ver Más'}
+        </LinkButtonBlue>
       </ExperienceDescription>
     </ExperiencesWrapper>
   );
