1ea5fbea75d4d68ca36cf47fba73f6154dc16d45
Arnold Restrepo <restrepo_arnold@outlook.com>
2020-05-14 18:14:10 -0500

    Contact Form  Translate into English

--- src/components/ContactForm/index.jsx
+++ src/components/ContactForm/index.jsx
@@ -1,27 +1,46 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { Context } from '../../Context';
 import { ButtonBlue } from '../../styles/Buttons';
 import { Form, Title, FormControl, Subtitle } from './style';
 
-const ContactForm = () => (
-  <Form action='#'>
-    <Title>Escríbe tu mensaje</Title>
-    <Subtitle>Pronto nos comunicaremos contigo</Subtitle>
-    <FormControl>
-      <label htmlFor='message'>Tu Mensaje:</label>
-      <textarea name='message' id='message' placeholder='Mi Mensaje...' />
-    </FormControl>
-    <FormControl>
-      <label htmlFor='name'>Nombre</label>
-      <input type='text' name='name' id='name' placeholder='Tu Nombre...' />
-    </FormControl>
-    <FormControl>
-      <label htmlFor='email'>Email</label>
-      <input type='email' name='email' id='email' placeholder='Tu Email...' />
-    </FormControl>
-    <FormControl>
-      <ButtonBlue type='submit'>Enviar</ButtonBlue>
-    </FormControl>
-  </Form>
-);
+const ContactForm = () => {
+  const { isEnglish } = useContext(Context);
+  return (
+    <Form action='#'>
+      <Title>{isEnglish ? 'Read your Message' : 'Escríbe tu mensaje'}</Title>
+      <Subtitle>
+        {isEnglish
+          ? 'We will contact you soon'
+          : 'Pronto nos comunicaremos contigo'}
+      </Subtitle>
+      <FormControl>
+        <label htmlFor='message'>
+          {isEnglish ? 'Your Message' : 'Tu Mensaje:'}
+        </label>
+        <textarea
+          name='message'
+          id='message'
+          placeholder={isEnglish ? 'My Message' : 'Mi Mensaje...'}
+        />
+      </FormControl>
+      <FormControl>
+        <label htmlFor='name'>{isEnglish ? 'Name' : 'Nombre:'}</label>
+        <input
+          type='text'
+          name='name'
+          id='name'
+          placeholder={isEnglish ? 'My Name' : 'Mi nombre...'}
+        />
+      </FormControl>
+      <FormControl>
+        <label htmlFor='email'>Email</label>
+        <input type='email' name='email' id='email' placeholder='Email...' />
+      </FormControl>
+      <FormControl>
+        <ButtonBlue type='submit'>{isEnglish ? 'Send' : 'Enviar'}</ButtonBlue>
+      </FormControl>
+    </Form>
+  );
+};
 
 export default ContactForm;
